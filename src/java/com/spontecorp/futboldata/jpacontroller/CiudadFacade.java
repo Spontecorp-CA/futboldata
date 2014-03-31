be90c7353b914d69da67abf4de2e295558dc1e12
sponte03 <sponte03@SPONTE031.SPONTECORP.INTERNAL>
2014-03-31 17:24:51 -0430

    Errores vARIOS Crear Pais

---
+++ src/java/com/spontecorp/futboldata/jpacontroller/CiudadFacade.java
@@ -0,0 +1,20 @@
+/*
+ * Derechos Reservados Spontecorp, C.A. 2014
+ * 
+ */
+
+package com.spontecorp.futboldata.jpacontroller;
+
+import com.spontecorp.futboldata.entity.Ciudad;
+
+/**
+ *
+ * @author sponte03
+ */
+public class CiudadFacade extends AbstractFacade<Ciudad>{
+
+    public CiudadFacade(Class<Ciudad> entityClass) {
+        super(entityClass);
+    }
+    
+}
--- src/java/com/spontecorp/futboldata/viewcontroller/LocalidadBean.java
+++ src/java/com/spontecorp/futboldata/viewcontroller/LocalidadBean.java
@@ -4,17 +4,22 @@
  */
 package com.spontecorp.futboldata.viewcontroller;
 
+import com.spontecorp.futboldata.entity.Ciudad;
 import com.spontecorp.futboldata.entity.Pais;
+import com.spontecorp.futboldata.jpacontroller.CiudadFacade;
 import com.spontecorp.futboldata.jpacontroller.PaisFacade;
 import com.spontecorp.futboldata.jpacontroller.PaisJpaController;
 import com.spontecorp.futboldata.jpacontroller.extensions.PaisJpaExt;
 import com.spontecorp.futboldata.utilities.Util;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import javax.faces.model.SelectItem;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 /**
@@ -26,18 +31,30 @@ import javax.persistence.EntityManagerFactory;
 public class LocalidadBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
-    private Pais pais;
-    private DataModel items = null;
 
+    private Pais pais;
+    private Ciudad ciudad;
+    private DataModel items = null;
+    private DataModel itemsCiudad = null;
+
+    private final CiudadFacade controllerCiudad;
     private final PaisFacade controllerPais;
     private final transient EntityManagerFactory emf = Util.getEmf();
-    
+
     /**
      * Creates a new instance of LocalidadBean
      */
     public LocalidadBean() {
         controllerPais = new PaisFacade(Pais.class);
+        controllerCiudad = new CiudadFacade(Ciudad.class);
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
     }
 
     public Pais getPais() {
@@ -46,6 +63,15 @@ public class LocalidadBean implements Serializable {
 
     public void setPais(Pais pais) {
         this.pais = pais;
+    }
+
+    public DataModel getItemsCiudad() {
+
+        if (itemsCiudad == null) {
+            itemsCiudad = new ListDataModel(controllerCiudad.findAll());
+        }
+        return itemsCiudad;
+
     }
 
     public DataModel getItems() {
@@ -66,32 +92,81 @@ public class LocalidadBean implements Serializable {
         items = null;
     }
 
+    private void recreateModelCiudad() {
+        itemsCiudad = null;
+    }
+
     public void setItems(DataModel items) {
         this.items = items;
+    }
+        public void setItemsCiudad(DataModel items) {
+        this.itemsCiudad = items;
     }
 
     public SelectItem[] getPaisAvailable() {
         return Util.getSelectItems(controllerPais.paisxNombre());
     }
 
-        public Pais getSelected() {
+    public Pais getSelectedPais() {
         if (pais == null) {
             pais = new Pais();
         }
         return pais;
     }
+    
+        public String create() {
+        try {
+            if (controllerCiudad.find(ciudad.getCiudad())==null) {
+                Util.addErrorMessage("Ciudad ya existente, coloque otro");
+                return null;
+            } else {
+                ciudad.setPaisId(pais);
+                controllerCiudad.create(ciudad);
+                Util.addSuccessMessage("Ciudad creada con éxito");
+               return prepareCreate();
+            }
+        } catch (Exception e) {
+            Util.addErrorMessage(e, "Error al crear la ciudad");
+            return null;
+        }
+    }
+        public Ciudad getSelectedCiudad() {
+        if (ciudad == null) {
+            ciudad = new Ciudad();
+        }
+        return ciudad;
+    }
+
+        public String prepareList() {
+        recreateModelCiudad();
+        return "listLocalidad";
+    }    
     public String returnAdminPage() {
         return "adminPage";
     }
-    
-        public String prepareCreate() {
+
+    public String prepareCreate() {
+        ciudad = new Ciudad();
         pais = new Pais();
-        return "createLocalidad";
+        return "createCiudad";
     }
-        
-        public String prepareEdit() {
+
+    public String prepareEdit() {
         pais = (Pais) getItems().getRowData();
         return "editLocalidad";
     }
-}
 
+    public void persist(Object object) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+}
