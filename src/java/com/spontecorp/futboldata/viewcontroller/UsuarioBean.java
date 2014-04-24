1e563a3609865340076ba35dc4ee13e826f9f6b3
jgcastillo <jgcastillo@SPONTE08-PC.SPONTECORP.INTERNAL>
2014-04-24 16:42:38 -0430

    Se actualizó la forma del user

--- src/java/com/spontecorp/futboldata/viewcontroller/UsuarioBean.java
+++ src/java/com/spontecorp/futboldata/viewcontroller/UsuarioBean.java
@@ -10,14 +10,13 @@ import com.spontecorp.futboldata.jpacontroller.UserFacade;
 import com.spontecorp.futboldata.utilities.SecurePassword;
 import com.spontecorp.futboldata.utilities.Util;
 import java.io.Serializable;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
-
-import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import javax.faces.model.SelectItem;
 import javax.inject.Named;
-import javax.persistence.EntityManagerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,48 +30,61 @@ public class UsuarioBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private User user;
-    private DataModel items = null;
+//    private User user;
+//    private DataModel items = null;
+    private List<User> items = null;
+    private User selected;
 
     //private final UserJpaExt controllerUser;
     private final UserFacade controllerUser;
     private final PerfilFacade controllerPerfil;
-    private final transient EntityManagerFactory emf; 
-    private final String contextPath;
+    //private final transient EntityManagerFactory emf; 
+    //private final String contextPath;
     private boolean changingPassword = false;
 
     private static final Logger logger = LoggerFactory.getLogger(UsuarioBean.class);
 
     public UsuarioBean() {
-        emf = Util.getEmf();
+        //emf = Util.getEmf();
         controllerUser = new UserFacade();
         controllerPerfil = new PerfilFacade();
-        contextPath = FacesContext.getCurrentInstance().getExternalContext().getContextName();
+        //contextPath = FacesContext.getCurrentInstance().getExternalContext().getContextName();
     }
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
 
     public User getSelected() {
-        if (user == null) {
-            user = new User();
-        }
-        return user;
+//        if (user == null) {
+//            user = new User();
+//        }
+        return selected;
+    }
+    
+    public void setSelected(User selected){
+        this.selected = selected;
     }
 
     public boolean isChangingPassword() {
         return changingPassword;
     }
 
-    public DataModel getItems() {
-        if (items == null) {
-            //items = new ListDataModel(controllerUser.findUserEntities());
-            items = new ListDataModel(controllerUser.findAll());
+//    public DataModel getItems() {
+//        if (items == null) {
+//            //items = new ListDataModel(controllerUser.findUserEntities());
+//            items = new ListDataModel(controllerUser.findAll());
+//        }
+//        return items;
+//    }
+    
+    public List<User> getItems(){
+        if(items == null){
+            items = controllerUser.findAll();
         }
         return items;
     }
@@ -96,56 +108,63 @@ public class UsuarioBean implements Serializable {
         return "/admin/adminPage";
     }
 
-    public String prepareCreate() {
-        user = new User();
-        return "create";
+//    public String prepareCreate() {
+//        user = new User();
+//        return "create";
+//    }
+    
+    public User prepareCreate(){
+        selected = new User();
+        return selected;
     }
 
-    public String create() {
+    public void create() {
         try {
-            if (controllerUser.findUsuario(user.getUsuario()) != null) {
+            if (controllerUser.findUsuario(selected.getUsuario()) != null) {
                 Util.addErrorMessage("Usuario ya existente, coloque otro");
-                return null;
+                //return null;
             } else {
-                user.setPassword(SecurePassword.encript(user.getPassword()));
-                controllerUser.create(user);
+                selected.setPassword(SecurePassword.encript(selected.getPassword()));
+                controllerUser.create(selected);
                 Util.addSuccessMessage("Usuario creado con éxito");
-                return prepareCreate();
+                //return prepareCreate();
+                items = null;
             }
         } catch (Exception e) {
             Util.addErrorMessage(e, "Error al crear el usuario");
-            return null;
+            //return null;
         }
     }
     
-    public String prepareEdit() {
-        user = (User) getItems().getRowData();
-        return "edit";
-    }
+//    public String prepareEdit() {
+//        user = (User) getItems().getRowData();
+//        return "edit";
+//    }
 
-    public String edit(){
+    public void edit(){
         try {
-            if(controllerUser.findUsuario(user.getUsuario()) == null ){
+            if(controllerUser.findUsuario(selected.getUsuario()) == null ){
                 Util.addErrorMessage("Usuario no existente");
-                return prepareList();
+                //return prepareList();
             } else {
                 if(changingPassword){
-                    user.setPassword(SecurePassword.encript(user.getPassword()));
+                    selected.setPassword(SecurePassword.encript(selected.getPassword()));
                 }
-                controllerUser.edit(user);
+                controllerUser.edit(selected);
                 Util.addSuccessMessage("Usuario editado con éxito");
                 changingPassword = false;
-                return prepareList();
+                //return prepareList();
+                items = null;
             }
         } catch (Exception e) {
             Util.addErrorMessage(e, "Error al editar el usuario");
-            return null;
+            //return null;
         }
     }
     
     public String preparePasswordChange(){
-        user = (User) getItems().getRowData();
-        changingPassword = true;
+//        selected = (User) getItems().getRowData();
+//        changingPassword = true;
         return "edit";
     }
             
