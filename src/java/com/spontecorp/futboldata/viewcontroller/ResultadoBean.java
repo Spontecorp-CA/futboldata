b3e08b84e6cb599e3798909b4328d03dd16ec434
sponte03 <sponte03@SPONTE031.SPONTECORP.INTERNAL>
2014-06-18 15:51:13 -0430

    Trabajando con los detalles de resultado

--- src/java/com/spontecorp/futboldata/viewcontroller/ResultadoBean.java
+++ src/java/com/spontecorp/futboldata/viewcontroller/ResultadoBean.java
@@ -2,12 +2,13 @@
  * Derechos Reservados Spontecorp, C.A. 2014
  * 
  */
-
 package com.spontecorp.futboldata.viewcontroller;
 
 import com.spontecorp.futboldata.entity.Convocado;
 import com.spontecorp.futboldata.entity.Convocatoria;
+import com.spontecorp.futboldata.entity.EquipoHasJugador;
 import com.spontecorp.futboldata.entity.Partido;
+import com.spontecorp.futboldata.jpacontroller.ConvocatoriaFacade;
 import com.spontecorp.futboldata.jpacontroller.PartidoFacade;
 import com.spontecorp.futboldata.utilities.Util;
 import java.io.Serializable;
@@ -20,25 +21,61 @@ import javax.inject.Named;
  */
 @Named("resultadoBean")
 @SessionScoped
-public class ResultadoBean implements Serializable{
-private static final long serialVersionUID = 1L;  
+public class ResultadoBean implements Serializable {
 
-private Partido partido;
-private final PartidoFacade partidoFacade;
-private Convocado convocado;
-private Convocatoria convocatoria;
+    private static final long serialVersionUID = 1L;
 
-    
+    private Partido partido;
+    private Convocado convocado;
+    private Convocatoria convocatoriaVisitante;
+    private Convocatoria convocatoriaLocal;
+    private EquipoHasJugador equipoHasJugador;
+    private final PartidoFacade partidoFacade;
+    private final ConvocatoriaFacade convocatoriaFacade;
 
-
-
-/**
+    /**
      * Creates a new instance of ResultadoBean
      */
-               
-        
     public ResultadoBean() {
         partidoFacade = new PartidoFacade();
+        convocatoriaFacade = new ConvocatoriaFacade();
+    }
+
+    public Convocatoria getConvocatoriaLocal() {
+        if (convocatoriaLocal == null) {
+            convocatoriaLocal = convocatoriaFacade.getConvocatoria(partido, partido.getEquipoLocalId());
+            if (convocatoriaLocal == null) {
+                convocatoriaLocal = new Convocatoria();
+                convocatoriaLocal.setEquipoId(partido.getEquipoLocalId());
+                convocatoriaLocal.setPartidoId(partido);
+                convocatoriaFacade.edit(convocatoriaLocal);
+            }
+
+        }
+        return convocatoriaLocal;
+    }
+
+    public Convocatoria getConvocatoriaVisitante() {
+        if (convocatoriaVisitante == null) {
+            convocatoriaVisitante = convocatoriaFacade.getConvocatoria(partido, partido.getEquipoVisitanteId());
+            if (convocatoriaVisitante == null) {
+                convocatoriaVisitante = new Convocatoria();
+                convocatoriaVisitante.setEquipoId(partido.getEquipoLocalId());
+                convocatoriaVisitante.setPartidoId(partido);
+                convocatoriaFacade.edit(convocatoriaVisitante);
+            }
+
+        }
+        return convocatoriaVisitante;
+    }
+
+    
+    public EquipoHasJugador getEquipoHasJugador() {
+        return equipoHasJugador;
+    }
+
+    public void setEquipoHasJugador(EquipoHasJugador equipoHasJugador) {
+        this.equipoHasJugador = equipoHasJugador;
     }
 
     public Partido getPartido() {
@@ -48,13 +85,14 @@ private Convocatoria convocatoria;
     public void setPartido(Partido partido) {
         this.partido = partido;
     }
-    public void guardar(){
+
+    public void guardar() {
         partidoFacade.edit(partido);
         Util.addSuccessMessage("Se edito con exito");
 
-        
     }
-    public String gotoResultadoPage(Partido partido){
+
+    public String gotoResultadoPage(Partido partido) {
         this.partido = partido;
         return "/admin/liga/temporadas/resultado/detallepartido??faces-redirect=true";
     }
